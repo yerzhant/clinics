fa3a9c74055fd6eb7da2b7d2fdff72c40c216708
Yerzhan <yerzhan>
2016-04-10 12:29:28 +0600

    db schema corrections

--- db.sql
+++ db.sql
@@ -1,6 +1,14 @@
 -- psql -U postgres -h localhost -f db.sql
 
+drop database if exists clinics;
+
+drop user if exists clinics_web;
+drop user if exists clinics;
+
 create user clinics password 'xxx';
+create user clinics_web password 'xxx';
+
 create database clinics owner clinics;
 
-create user clinics_web password 'xxx';
+\c clinics
+drop schema public;
